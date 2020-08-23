5c24234bfa6e7122cf5065573613aa8518678526
Rafael Freire <rafaelsilvafreire2@gmail.com>
2020-08-23 18:33:21 -0300

    added delete for atendimentos

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -24,4 +24,10 @@ module.exports = app => {
 
         atendimento.altera(id, valores, res)
     })
+
+    app.delete('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+
+        atendimento.deleta(id, res)
+    })
 }
